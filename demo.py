264e7657e599d6c857fef94fe47f20fecd54c7e8
charmantemort <147530843+charmantemortt@users.noreply.github.com>
2024-10-28 17:13:26 +0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,20 +1,20 @@
-while True:
-    print("1")
-    print("2")
-    print("3")
-    print("4")
-    TaskList = (int(input("Выберите задачу ")))
+list_tasks = []
 
-    if TaskList:
-        a = input()
+while True:
+    TaskList = int(input("Выберите действие:\n 1. Добавить задачу\n 2. Просмотреть список задач \n 3. Удалить задачу \n 4. Выйти из программы"))
+    if TaskList == 1:
+        a = input("Добавить задачу ")
+        list_tasks.append(a)
         print("Задача добавлена!")
-    elif TaskList:
-        my_list = 'Купить продукты'
-        print(my_list)
-    elif TaskList:
-        my_list = ['1, 2, 3, 4']
-        my_list.remove(1)
-    elif TaskList:
-        exit()
+    elif TaskList == 2:
+        print("Список задач")
+        print(list_tasks)
+    elif TaskList == 3:
+        del_value = int(input("Выберите номер задачи для удаления: "))
+        list_tasks.pop(del_value - 1)
+        print(list_tasks)
+    elif TaskList == 4:
+        print("Программа завершена")
+        break
     else:
         print("Error")
